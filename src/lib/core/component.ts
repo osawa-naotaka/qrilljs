19591a744b6a3038bb250b14982e46e4536215c5
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-10-12 12:03:26 +0900

    stop re-nameing component function for desginator.

--- src/lib/core/component.ts
+++ src/lib/core/component.ts
@@ -35,13 +35,11 @@ export function element<K extends Tag | qrillTag>(store: Store, tag?: K, arg: El
     const dot_name = `.${name_with_hash}`;
     const class_name = arg.class === undefined ? [] : typeof arg.class === "string" ? [arg.class] : arg.class;
     return Object.assign(
-        {
-            [dot_name]: (attribute: AttributeOf<K>, ...children: HNode[]) => ({
-                tag: tag || ("div" as const),
-                attribute: addClassInRecord(attribute, [name_with_hash, ...class_name]),
-                children,
-            }),
-        }[dot_name],
+        (attribute: AttributeOf<K>, ...children: HNode[]) => ({
+            tag: tag || ("div" as const),
+            attribute: addClassInRecord(attribute, [name_with_hash, ...class_name]),
+            children,
+        }),
         {
             designator: dot_name,
         },
@@ -61,15 +59,9 @@ export type HComponentFnArg<T> = T & { class?: string | string[]; id?: string; c
 // if name_fn is string, it refers html elemen name like html, body, p..., so we don't use dottend name for that.
 export function component<K, T>(name_fn: HComponentFn<K> | string, component_fn: HComponentRawFn<T>): HComponentFn<T> {
     const component_name = typeof name_fn === "string" ? `.${name_fn}` : name_fn.designator;
-    return Object.assign(
-        {
-            [component_name]: (argument: HComponentFnArg<T>, ...children: HNode[]) =>
-                component_fn(argument, ...children),
-        }[component_name],
-        {
-            designator: component_name,
-        },
-    );
+    return Object.assign((argument: HComponentFnArg<T>, ...children: HNode[]) => component_fn(argument, ...children), {
+        designator: component_name,
+    });
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: HAnyComponent uses only for function.name
@@ -81,10 +73,7 @@ export function as<T>(class_name: string, fn: HComponentFn<T>): HComponentFn<T> 
     const Class = gt("class");
     const dot_name = `.${class_name}`;
     return Object.assign(
-        {
-            [dot_name]: (argument: HComponentFnArg<T>, ...children: HNode[]) =>
-                Class({ class: class_name }, fn(argument, ...children)),
-        }[dot_name],
+        (argument: HComponentFnArg<T>, ...children: HNode[]) => Class({ class: class_name }, fn(argument, ...children)),
         {
             designator: dot_name,
         },
