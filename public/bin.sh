1de52bffb4c9966018c3cbdc47543c814233381c
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-10-20 14:06:55 +0900

    deno support.

--- public/bin.sh
+++ public/bin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-if command -v bun >/dev/null 2>&1; then
-    exec bun -e='import { main } from "qrilljs/bin"; await main(process.argv.slice(1));' "$@"
-elif command -v deno >/dev/null 2>&1; then
+if command -v deno >/dev/null 2>&1; then
     exec deno eval 'import { main } from "qrilljs/bin"; await main(process.argv.slice(2));' "$@" 
 elif command -v tsx >/dev/null 2>&1; then
     exec node --import=tsx --eval 'import { main } from "qrilljs/bin"; await main(process.argv.slice(1));' "$@"
+elif command -v bun >/dev/null 2>&1; then
+    exec bun -e='import { main } from "qrilljs/bin"; await main(process.argv.slice(1));' "$@"
 else
     echo "Error: Neither bun, tsx, is installed" >&2
     exit 1
